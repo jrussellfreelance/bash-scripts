211425b93cc1593e40626c783af557fd745fdb6d
Jesse Russell <jrussellfreelance@gmail.com>
2019-10-23 19:51:43 -0400

    Updated nginx scripts

--- nginx/new-phpfpm.sh
+++ nginx/new-phpfpm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "An Nginx reverse proxy config file generator"
+# Designed for Ubuntu 18.04
+echo "An Nginx php-fpm config file generator"
 # Grab variables
 while [[ -z "$APPNAME" ]]
 do
@@ -11,21 +12,33 @@ do
 done
 while [[ -z "$PORT" ]]
 do
-  read -p "Port: " PORT
+  read -p "PHP-FPM Port: " PORT
+done
+while [[ -z "$ROOTPATH" ]]
+do
+  read -p "Path to website files: " ROOTPATH
 done
 # Start creating config file
 echo "Creating nginx config..."
 # Copy contents of default file into new config file
-cp reverseproxy /etc/nginx/sites-available/$APPNAME
+cp ../templates/nginx/phpfpm /etc/nginx/sites-available/$APPNAME
 # Link nginx config to sites-enabled folder
 ln -s /etc/nginx/sites-available/$APPNAME /etc/nginx/sites-enabled/$APPNAME
 # Replace placeholder values from default file with values from read commands
 echo "Replacing values..."
 sed -i 's/sub.domain.ext/'$DOMAIN'/g' /etc/nginx/sites-available/$APPNAME
-sed -i 's/porthere/'$PORT'/g' /etc/nginx/sites-available/$APPNAME
+sed -i 's/900x/'$PORT'/g' /etc/nginx/sites-available/$APPNAME
 sed -i 's/appname/'$APPNAME'/g' /etc/nginx/sites-available/$APPNAME
-# Restart nginx
-echo "Restarting nginx..."
+sed -i 's/root_path_here/"$ROOTPATH"/g' /etc/nginx/sites-available/$APPNAME
+# Create php-fpm pool
+echo "Creating PHP-FPM config..."
+cp ../templates/php-fpm/phpfpm.conf /etc/php/7.2/fpm/pool.d/$APPNAME.conf
+echo "Replacing values..."
+sed -i 's/pool_name_here/'$APPNAME'/g' /etc/php/7.2/fpm/pool.d/$APPNAME.conf
+sed -i 's/900x/'$PORT'/g' /etc/php/7.2/fpm/pool.d/$APPNAME.conf
+# Restart services
+echo "Restarting services..."
+systemctl restart php7.2-fpm
 systemctl restart nginx
 # Run certbot to generate CA certified ssl certificate
 echo "Running certbot..."
