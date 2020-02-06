c7459ebe18e825c7bf212a9c2f44edcc3a45bb42
Jesse Russell <jrussellfreelance@gmail.com>
2020-02-06 03:15:14 +0000

    added recursive git script

--- legacy_migrate/apps/install-sublime-text.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-apt update
-apt -y install sublime-text
--- staging_migrate/deploy-asciinema.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Deploy Asciinema on a server
-git clone --recursive https://github.com/asciinema/asciinema-server.git
-cd asciinema-server
-git checkout master
-git checkout -b underdog master
-cp .env.production.sample .env.production
-echo nano .env.production
