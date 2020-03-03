c0a71e042f381629f52814ab61f34a9d561dced3
Jesse Russell <jrussellfreelance@gmail.com>
2020-03-03 13:58:16 -0500

    updated setup-samba

--- raspberrypi-scripts/setup-samba.sh
+++ raspberrypi-scripts/setup-samba.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 # Designed on Ubuntu 18.04 32-bit for a raspberrypi
 # run as sudo
-# Grab mountpoint, mount path and mount user
+# Grab mountpoint, mount PTH and mount USR
 echo " >> printing available drives for you << "
 fdisk -l
 while [[ -z "$MOUNT" ]]
 do
 read -p "enter mount point >> " MOUNT
 done
-while [[ -z "$PATH" ]]
+while [[ -z "$PTH" ]]
 do
-read -p "enter mount path  >> " PATH
+read -p "enter mount PTH  >> " PTH
 done
-while [[ -z "$USER" ]]
+while [[ -z "$USR" ]]
 do
-read -p "enter mount user  >> " USER
+read -p "enter mount USR  >> " USR
 done
-mkdir -p $PATH
+mkdir -p $PTH
 # Mount point
-mount -t ext4 $MOUNT $PATH
-# Add user
-useradd -d $PATH -g sudo $USER
+mount -t ext4 $MOUNT $PTH
+# Add USR
+USRadd -d $PTH -g sudo $USR
 # Install samba
 apt update
 apt -y install samba
-# Set password for new user, it will prompt you
-smbpasswd -a $USER
+# Set password for new USR, it will prompt you
+smbpasswd -a $USR
 echo "[mount]
-path = $PATH
-valid users = $USER
+PTH = $PTH
+valid USRs = $USR
 read only = no
 " >> /etc/samba/smb.conf
 # Restart smb service
 systemctl restart smbd
 # Add to fstab
-echo "$MOUNT           $PATH    ext4       rw,defaults         0      0" >> /etc/fstab
+echo "$MOUNT           $PTH    ext4       rw,defaults         0      0" >> /etc/fstab
