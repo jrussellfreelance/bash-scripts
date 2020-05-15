178f656409652993b86d708d4aff20987a3378dd
Jesse Russell <jrussellfreelance@gmail.com>
2020-05-15 05:17:49 +0000

    script updates

---
+++ install/install-basics.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script installs some basic Ubuntu goodies.
+apt install -y \
+git \
+nano \
+curl \
+wget \
+unzip \
+net-tools \
+build-essential \
+apt-transport-https \
+software-properties-common
---
+++ install/install-ols.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Designed for Ubuntu
+# This script installs and configures OpenLiteSpeed.
+# One click install script
+wget --no-check-certificate https://raw.githubusercontent.com/litespeedtech/ols1clk/master/ols1clk.sh
+bash ols1clk.sh
+rm -f ols1clk.sh
+
+# OLS Admin Password
+OLS_PASSWORD=$(openssl rand -base64 12)
+/usr/local/lsws/admin/misc/admpass.sh << EOF
+admin
+$OLS_PASSWORD
+$OLS_PASSWORD
+EOF
+
+# Restart OpenLiteSpeed
+systemctl restart lsws
+
+# Get IP Address
+IP=$(curl -s icanhazip.com)
+
+echo " Admin URL ➜ http://$IP:7080 "          # Prints link to Admin Panel
+echo " OLS User  ➜ admin "                # Prints username
+echo " Password  ➜ $OLS_PASSWORD "            # Prints password
