e018e25d9c53df680fb961e72d6be213fb6f9a0b
Jesse <jrussellfreelance@gmail.com>
2020-06-17 01:43:59 -0400

    updated nginx proxy sccript

--- web/create-nginx-proxy.sh
+++ web/create-nginx-proxy.sh
@@ -9,22 +9,13 @@ while [[ -z "$appport" ]]
 do
     read -p "port   >> " appport
 done
-sudo apt install nginx python-certbot-nginx -y
-certbot certonly --rsa-key-size 2048 --standalone --agree-tos --no-eff-email --email jesse@jrussell.io -d ${domain}
+apt install nginx python-certbot-nginx -y
 cd /etc/nginx/sites-available/
 touch $domain
 echo "
 server {
-       listen 80;
-       server_name ${domain};
-       return 301 https://"'$server_name$request_uri'";
-}
-server {
-    listen 443 ssl;
-    listen [::]:443 ssl;
+    listen 80;
     server_name ${domain};
-    ssl_certificate /etc/letsencrypt/live/${domain}/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/${domain}/privkey.pem;
     location / {
       proxy_pass http://127.0.0.1:${appport};
       proxy_set_header X-Forwarded-For "'$remote_addr'";
@@ -33,6 +24,6 @@ server {
 " > $domain
 ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/
 nginx -t
-sudo systemctl restart nginx
-sudo systemctl enable nginx
+systemctl restart nginx
+systemctl enable nginx
 echo Finished!
