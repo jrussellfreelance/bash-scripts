c352a272bd02599425afe207ad7322f880c18d51
Jesse <jrussellfreelance@gmail.com>
2020-06-17 00:12:18 -0400

    updated nextcloud script

--- deploy/deploy-nextcloud-docker.sh
+++ deploy/deploy-nextcloud-docker.sh
@@ -8,7 +8,7 @@ done
 mkdir -p nextcloud/{apps,config,data}
 cd nextcloud
 docker run  --name nextcloud -d \
--v ./:/var/www/html \
+-v ${pwd}/:/var/www/html \
 -v apps:/var/www/html/custom_apps \
 -v config:/var/www/html/config \
 -v data:/var/www/html/data \
