afb6ac859b8afb9ea63d25d49a1c949552033b04
Jesse <jrussellfreelance@gmail.com>
2020-06-17 21:50:40 -0400

    updated web scripts

--- web/create-nginx-proxy.sh
+++ web/create-nginx-proxy.sh
@@ -17,7 +17,12 @@ server {
     listen [::]:80;
     server_name ${domain};
     location / {
-      proxy_pass http://127.0.0.1:${appport};
+        proxy_pass http://127.0.0.1:${appport};
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
     }
 }
 " > $domain
