897e9ff8fe0f8da08da6aeaf2564163058633a1c
Jesse Russell <jrussellfreelance@gmail.com>
2022-12-10 04:26:04 -0500

    Update deploy-wordpress-docker.sh

--- deploy/deploy-wordpress-docker.sh
+++ deploy/deploy-wordpress-docker.sh
@@ -6,13 +6,17 @@ while [[ -z "$hostport" ]]
 do
     read -p "host port >> " hostport
 done
+while [[ -z "$phpport" ]]
+do
+    read -p "phpmyadmin port >> " hostport
+done
 while [[ -z "$domain" ]]
 do
     read -p "wordpress domain >> " domain
 done
 # Generate passwords
-mysql_db_pwd=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
-mysql_root_pwd=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
+mysql_db_pwd=$(date +%s | sha256sum | base64 | head -c 14 ; echo)
+mysql_root_pwd=$(date +%s | sha256sum | base64 | head -c 14 ; echo)
 
 # Create linked directories
 mkdir -p wordpress/$domain/wp-content
@@ -20,51 +24,84 @@ cd wordpress/$domain
 
 # Create uploads.ini
 cat <<'EOF' > uploads.ini
-file_uploads = On
-memory_limit = 64M
-upload_max_filesize = 64M
-post_max_size = 64M
-max_execution_time = 600
+memory_limit=2048M
+upload_max_filesize=2048M
+post_max_size=2048M
+max_execution_time=300
 EOF
 
 # Create docker-compose file
 cat <<'EOF' > docker-compose.yml
-version: '3.3'
-
+version: '2'
 services:
-   db:
-     image: mysql:5.7
-     volumes:
-       - db_data:/var/lib/mysql
-     restart: always
-     environment:
-       MYSQL_ROOT_PASSWORD: @@@mysql_root_pwd@@@
-       MYSQL_DATABASE: wordpress
-       MYSQL_USER: wordpress
-       MYSQL_PASSWORD: @@@mysql_db_pwd@@@
-
    wordpress:
      depends_on:
        - db
-     image: wordpress:latest
-     ports:
-       - "@@@hostport@@@:80"
+     image: wordpress
      restart: always
      environment:
        WORDPRESS_DB_HOST: db:3306
        WORDPRESS_DB_USER: wordpress
        WORDPRESS_DB_PASSWORD: @@@mysql_db_pwd@@@
        WORDPRESS_DB_NAME: wordpress
+     ports:
+       - "@@@hostport@@@:80"
+     networks:
+       - wordpress_network
      working_dir: /var/www/html
      volumes:
        - ./wp-content:/var/www/html/wp-content
        - ./uploads.ini:/usr/local/etc/php/conf.d/uploads.ini
+   db:
+     image: mariadb
+     restart: always
+     environment:
+       MYSQL_ROOT_PASSWORD: @@@mysql_root_pwd@@@
+       MYSQL_DATABASE: wordpress
+       MYSQL_USER: wordpress
+       MYSQL_PASSWORD: @@@mysql_db_pwd@@@
+     networks:
+       - wordpress_network
+     volumes:
+       - db_data:/var/lib/mysql
+
+   wordpress:
+     depends_on:
+       - db
+     image: wordpress:latest
+     restart: always
+     environment:
+       WORDPRESS_DB_HOST: db:3306
+       WORDPRESS_DB_USER: wordpress
+       WORDPRESS_DB_PASSWORD: @@@mysql_db_pwd@@@
+       WORDPRESS_DB_NAME: wordpress
+     ports:
+       - "@@@hostport@@@:80"
+     networks:
+       - wordpress_network
+     working_dir: /var/www/html
+     volumes:
+       - ./wp-content:/var/www/html/wp-content
+       - ./uploads.ini:/usr/local/etc/php/conf.d/uploads.ini
+   phpmyadmin:
+     depends_on:
+       - db
+     image: phpmyadmin
+     restart: always
+     environment:
+       PMA_HOST: db
+       MYSQL_ROOT_PASSWORD: @@@mysql_db_pwd@@@
+     ports:
+       - "@@@phpport@@@:80"
+     networks:
+       - wordpress_network
 volumes:
     db_data: {}
 EOF
 
 # replace placeholder values with bash variables
 sed -i "s/@@@hostport@@@/${hostport}/g" docker-compose.yml
+sed -i "s/@@@phpport@@@/${phpport}/g" docker-compose.yml
 sed -i "s/@@@mysql_root_pwd@@@/${mysql_root_pwd}/g" docker-compose.yml
 sed -i "s/@@@mysql_db_pwd@@@/${mysql_db_pwd}/g" docker-compose.yml
 
