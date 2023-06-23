0598715b238cdab5af2cfa03600eab75e235da2b
Jesse Russell <jrussellfreelance@gmail.com>
2023-06-23 03:26:45 -0400

    Update create-nginx-proxy.sh

--- web/create-nginx-proxy.sh
+++ web/create-nginx-proxy.sh
@@ -36,10 +36,10 @@ server {
     location / {
         proxy_pass ${proxypass};
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection 'upgrade';
-        proxy_set_header Host $host;
-        proxy_cache_bypass $http_upgrade;
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
     }
 }
 " > $domain
