ee4b540bd10896450e4217756cc3be20d4d13fce
Jesse <jrussellfrelance@gmail.com>
2023-06-30 07:01:54 -0400

    updated basics script

--- install/install-basics.sh
+++ install/install-basics.sh
@@ -3,10 +3,12 @@
 # This script installs some basic Ubuntu goodies.
 sudo apt install -y \
 git \
+vim \
 nano \
 curl \
 wget \
 unzip \
+expect \
 net-tools \
 build-essential \
 apt-transport-https \
