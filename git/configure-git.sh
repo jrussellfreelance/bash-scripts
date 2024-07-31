701d9e76beced42ae0f6db9a229e637af9a703c3
Jesse Russell <jrussellfreelance@gmail.com>
2024-07-31 06:32:28 -0400

    added gitweb config

--- git/configure-git.sh
+++ git/configure-git.sh
@@ -136,26 +136,26 @@ echo " --> configure-git.sh: complete ---"
     do
         read -p "  -> project description: " GIT_DESCRIPTION
     done
-    echo "   + git config gitweb.description $GIT_DESCRIPTION"
-    git config gitweb.description $GIT_DESCRIPTION
+    echo "   + git config gitweb.description '$GIT_DESCRIPTION'"
+    git config gitweb.description "$GIT_DESCRIPTION"
     while [[ -z "$GIT_CATEGORY" ]]
     do
         read -p "  -> project category: " GIT_CATEGORY
     done
-    echo "   + git configgitweb.category $GIT_CATEGORY"
-    git config gitweb.category $GIT_CATEGORY
+    echo "   + git config gitweb.category '$GIT_CATEGORY'"
+    git config gitweb.category "$GIT_CATEGORY"
     while [[ -z "$GIT_CLONEURL" ]]
     do
         read -p "  -> project cloneurl: " GIT_CLONEURL
     done
-    echo "   + git config gitweb.cloneurl $GIT_CLONEURL"
-    git config gitweb.cloneurl $GIT_CLONEURL
+    echo "   + git config gitweb.cloneurl '$GIT_CLONEURL'"
+    git config gitweb.cloneurl "$GIT_CLONEURL"
     while [[ -z "$GIT_OWNER" ]]
     do
         read -p "  -> project owner: " GIT_OWNER
     done
-    echo "   + git config gitweb.owner $GIT_OWNER"
-    git config gitweb.owner $GIT_OWNER
+    echo "   + git config gitweb.owner '$GIT_OWNER'"
+    git config gitweb.owner "$GIT_OWNER"
     echo " --> configure-git.sh: complete ---"
 ;;
 [9])
