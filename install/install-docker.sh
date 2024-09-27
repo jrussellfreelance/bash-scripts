c82ebd91d6319ef21f996e8eee4ec91ae410a846
Jesse Russell <jrussellfreelance@gmail.com>
2024-09-27 05:52:45 -0400

    updated scripts formatting

--- install/install-docker.sh
+++ install/install-docker.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # tested on Ubuntu
-# this script installs docker and docker-compose
+# this script installs docker
 
 # install dependencies
 sudo apt install -y ca-certificates curl uidmap
+
 # install docker using script
 curl -sSL https://get.docker.com | sudo bash
+
 # configure rootless docker
 dockerd-rootless-setuptool.sh install
--- install/install-vscodium.sh
+++ install/install-vscodium.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Tested on Ubuntu
 # This script installs VSCodium via apt
+
 # refresh apt sources and perform updates
 sudo apt update && sudo apt upgrade
+
 # add signing key and repository
 curl -fSsL https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/vscodium.gpg > /dev/null
 echo deb [signed-by=/usr/share/keyrings/vscodium.gpg] https://download.vscodium.com/debs vscodium main | sudo tee /etc/apt/sources.list.d/vscodium.list
+
 # refresh sources again and install
 sudo apt update && sudo apt install codium -y
